f1f5434b2528b9b297a7e1caa08f163c243b0f27
Jess Moran <jessmoran.dev@gmail.com>
2018-03-27 09:03:50 -0600

    new styles

--- js/app.js
+++ js/app.js
@@ -2,24 +2,66 @@ const inputLocation = document.querySelector("input");
 const btnSearch = document.querySelector("button");
 
 
-inputLocation.addEventListener("keyup",searchLocation);
 btnSearch.addEventListener("click",getPlace);
+$('#reset-btn').click(resetValues);
 
-function searchLocation (){
-  let city = inputLocation.value.toLowerCase();
-}
 
 function getPlace(){
-let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDCNqBEzPh4G-q_KdqMaXDcgrKeF0A2uE4`;
+  let city = inputLocation.value.toLowerCase();
+  console.log(city);
+  let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=AIzaSyASipcFbUQ-EPHlwc4zHazQKcJ443uTp5I';
 
    fetch(url)
        .then(response => response.json())
        .then(data => {
+          console.log(data);
            const lat = data.results[0].geometry.location.lat;
            const lng = data.results[0].geometry.location.lng;
-           todayWeather(lat,lng)
-              console.log(url);
+           todayWeather(lat,lng);
        })
 
-       .catch( e => console.log( 'Something went wrong' ) );
+       .catch( e => {console.log(e); console.log( 'Something went wrong' ) });
+
+}
+
+function todayWeather(lat, lng) {
+
+  $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/dbe165170a9d3843d322e095bd74e1ba/' + lat + ',' + lng
+    }).done(function(data) {
+      console.log(data);
+      displayValues(data);
+    })
+    .fail(function(error) {
+      console.log(error);
+    });
+
+}
+
+function displayValues(data) {
+  var current = data.currently;
+  var temp = ((current.temperature-32)*5)/9;
+  var apparentTemp = ((current.apparentTemperature-32)*5)/9;
+  var date = new Date(current.time*1000);
+
+  $('#temp').text(temp.toFixed(0));
+  $('#apparentTemp').text(apparentTemp.toFixed(0));
+  $('#summary').text(current.summary);
+  $('#time').text(date);
+
+  $('#w-forecast').show();
+  $('#background').hide();
+}
+
+function resetValues() {
+  $('#w-input').val('');
+  $('#w-forecast').hide();
+  $('#background').show();
+}
+
+function runScript(e) {
+    if (e.keyCode == 13) {
+        getPlace();
+        return false;
+    }
 }
